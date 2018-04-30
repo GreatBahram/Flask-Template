865fad2062c8b05d35766270e40d1b1398b0196e
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-04-30 14:00:34 +0430

    Add flask's file structure

---
+++ config.py
@@ -0,0 +1,37 @@
+class Config:
+    """
+    Common configurations
+    Put any configurations here that are common across all environments
+    """
+    DEBUG = False
+    TESTING = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+class DevelopmentConfig(Config):
+    """
+    Development configurations
+    """
+    DEBUG = True
+    SQLALCHEMY_ECHO = True
+    SECRET_KEY = "FakeK3y"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///database.db"
+
+
+class TestingConfig(Config):
+    """
+    Testing configurations
+    """
+    TESTING = True
+
+
+class ProductionConfig(Config):
+    """
+    Production configurations
+    """
+
+app_config = {
+        'development': DevelopmentConfig,
+        'production': ProductionConfig,
+        'testing': TestingConfig,
+        }
