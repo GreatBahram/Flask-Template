39857c48b33ea8274a63e1325ba82e2a651b4d09
Bahram Aghaei <aghaee.bahram@gmail.com>
2018-06-07 12:10:04 +0430

    Add custom error pages

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 # app/__init__.py
 
 # third-party imports
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # local imports
@@ -14,5 +14,21 @@ def create_app(config_name):
     app = Flask(__name__)
     app.config.from_object(app_config[config_name])
     db.init_app(app)
+
+    @app.route('/')
+    def homepage():
+        return "Hello dude, Welcome!"
+
+    @app.errorhandler(403)
+    def forbidden(error):
+        return render_template('errors/403.html', title='Forbidden'), 403
+
+    @app.errorhandler(404)
+    def page_not_found(error):
+        return render_template('errors/404.html', title='Page Not Found'), 404
+
+    @app.errorhandler(500)
+    def internal_server_error(error):
+        return render_template('errors/500.html', title='Server Error'), 500
 
     return app
